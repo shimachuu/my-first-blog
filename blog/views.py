fa7cc871eaf514a88e55a80959e34cbe8b3ed649
Ryosuke Surface <r-shimada0930@gmail.com>
2021-07-18 15:37:17 +0900

    HTMLにDjango ORM のクエリセットを動的に代入

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #リクエストというのは、インターネットを介してユーザから受け取った全ての情報が詰まったものです
+    return render(request, 'blog/post_list.html', {'posts':posts})
 
